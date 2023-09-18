97fefb2d4f0083dc2a798aa6ee7040e2f5d69a0b
Qasim Muhammad Akhtar <memon.qasim786@gmail.com>
2023-09-18 03:56:05 -0600

    updating python script

--- version.py
+++ version.py
@@ -3,4 +3,7 @@ import requests
 print(requests.__version__)
 
 r = requests.get("https://google.com")
-print(r.status_code)
+
+r = requests.get("https://raw.githubusercontent.com/qasim-tech/Cmput404-Lab1/master/version.py")
+
+print(r.text)
